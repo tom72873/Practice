1c556b541070eb9fca99d55f50799e25bcac2ed0
car <tom72873@gmail.com>
2021-12-03 11:19:12 +0800

    Changes

--- src/car/practice3/Maze.java
+++ src/car/practice3/Maze.java
@@ -1,6 +1,8 @@
 package car.practice3;
-
+import static java.lang.System.out;
+import java.lang.StringBuilder;
 public class Maze {
+     public int a=0;
      private int[][] maze={
             {2,2,2,2,2,2,2},
             {0,0,0,0,0,0,2},
@@ -10,7 +12,7 @@ public class Maze {
             {2,0,0,0,0,0,2},
             {2,2,2,2,2,0,2},
     };
-    private final int[][] mazeBU={
+    private int[][] mazeOut={
             {2,2,2,2,2,2,2},
             {0,0,0,0,0,0,2},
             {2,0,2,0,2,0,2},
@@ -19,40 +21,78 @@ public class Maze {
             {2,0,0,0,0,0,2},
             {2,2,2,2,2,0,2},
     };
-    private int nowPosX,nowPosY;
+    private int[][] record=new int[100][2];
+    private int[][] mk=new int[7][7];
     private int up=1,down=2,left=3,right=4;
     public Maze(){
-        nowPosX=0;
-        nowPosY=1;
-        maze[nowPosY][nowPosX]=1;
     }
     public void start(){
-        chooseRoad();
+        dfs(0,1);
     }
-    private void chooseRoad(){
-        if(nowPosY+1>=0&&maze[nowPosY+1][nowPosX]==0)
-            move(up);
-        if(nowPosY-1>=0&&maze[nowPosY-1][nowPosX]==0)
-            move(down);
-        if(nowPosX-1>=0&&maze[nowPosY][nowPosX-1]==0)
-            move(left);
-        if(nowPosX+1>=0&&maze[nowPosY][nowPosX+1]==0)
-            move(right);
-        if(maze[nowPosY+1][nowPosX]!=0&&maze[nowPosY-1][nowPosX]!=0&&maze[nowPosY][nowPosX-1]!=0&&maze[nowPosY][nowPosX+1]!=0);//如果是死路 倒推回去把路堵住
-    }
-    private void move(int safeRoad){
-        switch (safeRoad){
-            case 1:maze[++nowPosY][nowPosX]=1;
-            case 2:maze[--nowPosY][nowPosX]=1;
-            case 3:maze[nowPosY][--nowPosX]=1;
-            case 4:maze[nowPosY][++nowPosX]=1;
+    private void dfs(int nowPosX,int nowPosY){
+        /*for(int i=0;i<7;i++) {
+            for (int j = 0; j < 7; j++) {
+                out.print(maze[i][j]);
+            }
+            out.println();
         }
+        out.println();*/
+        if(nowPosX>7||nowPosY>7)
+            return;
+        if(nowPosX<0||nowPosY<0||mk[nowPosY][nowPosX]!=0)
+            return;
+        if(maze[nowPosY][nowPosX]==2)
+            return;
+        if(nowPosX==5&&nowPosY==6){
+            int temp=a;
+            record[a][0]=nowPosY;
+            record[a][1]=nowPosX;
+            a=0;
+            while(a!=temp+1){
+                mazeOut[record[a][0]][record[a][1]]=1;
+                a++;
+            }
+            for(int i=0;i<7;i++) {
+                for (int j = 0; j < 7; j++) {
+                    out.print(mazeOut[i][j]);
+                }
+                out.println();
+            }
+            return;
+        }
+        int temp=a;
+        record[a][0]=nowPosY;
+        record[a][1]=nowPosX;
+        a++;
+        mk[nowPosY][nowPosX]=1;
+        dfs(nowPosX+1,nowPosY);
+        dfs(nowPosX,nowPosY+1);
+        dfs(nowPosX,nowPosY-1);
+        dfs(nowPosX-1,nowPosY);
+        mk[nowPosY][nowPosX]=0;
+        a=temp;
     }
-    private void recoverMaze(){
+    /*private void chooseRoad(int nowPosX,int nowPosY){
+
+        move(1,nowPosX,nowPosY);
+        move(2,nowPosX,nowPosY);
+        move(3,nowPosX,nowPosY);
+        move(4,nowPosX,nowPosY);
+
+    }
+    private void move(int safeRoad,int nowPosX,int nowPosY){
+
+        maze[--nowPosY][nowPosX]=1;
+        maze[nowPosY][--nowPosX]=1;
+        maze[nowPosY][++nowPosX]=1;
+
+        dfs(nowPosX,nowPosY);
+    }*/
+    /*private void syncMaze(){
         for(int i=0;i<7;i++){
             for (int j=0;j<7;j++){
-                maze[i][j]=mazeBU[i][j];
+                mazeBU[i][j]=maze[i][j];
             }
         }
-    }
+    }*/
 }
